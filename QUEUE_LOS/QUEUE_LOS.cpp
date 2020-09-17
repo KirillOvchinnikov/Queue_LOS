8bb2cfadd4bd66a3fb0057f67723d9d5d82caf97
KirillOvchinnikov <kirill.ovchinnikov.1968@mail.ru>
2020-09-17 15:20:29 +0300

    fixed

--- QUEUE_LOS/QUEUE_LOS.cpp
+++ QUEUE_LOS/QUEUE_LOS.cpp
@@ -8,12 +8,14 @@ struct Node //описание узла списка
 };
 struct Queue //описание очереди
 {
+	int maxSize;
 	int size; //счетчик размера очереди
 	Node* first; //указатель на начало очереди
 	Node* last; //указатель на конец очереди
 };
-void Creation(Queue* Q) //создание очереди
+void Creation(Queue* Q, unsigned int maxSize) //создание очереди
 {
+	Q->maxSize = maxSize;
 	Q->first = new Node;
 	Q->first->next = NULL;
 	Q->last = Q->first;
@@ -24,6 +26,11 @@ bool IsEmpty(Queue* Q) //проверка очереди на пустоту
 	if (Q->first == Q->last) return true;
 	else return false;
 }
+bool IsFull(Queue* Q) //проверка очереди на пустоту
+{
+	if (Q->size == Q->maxSize) return true;
+	else return false;
+}
 string Top(Queue* Q) //вывод начального элемента
 {
 	return Q->first->next->data;
@@ -31,13 +38,22 @@ string Top(Queue* Q) //вывод начального элемента
 void Add(Queue* Q) //добавление элемента
 {
 	string value;
-	cout << "\nЗначение > "; cin >> value;
+	cout << "\nЗначение: "; cin >> value;
 	Q->last->next = new Node;
 	Q->last = Q->last->next;
 	Q->last->data = value; //добавление элемента в конец
 	Q->last->next = NULL; //обнуление указателя на следующий элемент
-	Q->size++;
-	cout << "\nЭлемент добавлен\n\n";
+	if (Q->size == Q->maxSize)
+	{
+		cout << "\nОчередь полна, добавление невозможно\n\n";
+	}
+	else
+	{
+		Q->size++;
+		cout << "\nЭлемент добавлен\n\n";
+	}
+
+	
 }
 void Delete(Queue* Q) //удаление элемента
 {
@@ -48,8 +64,12 @@ void Delete(Queue* Q) //удаление элемента
 void main() //главная функция
 {
 	setlocale(LC_ALL, "Rus");
+	unsigned int mSize;
 	Queue Q;
-	Creation(&Q);
+	cout << "Введите размер очереди: ";
+	cin >> mSize;
+	cout << endl;
+	Creation(&Q, mSize);
 	char number;
 	do
 	{
@@ -57,8 +77,11 @@ void main() //главная функция
 		cout << "2. Удалить элемент" << endl;
 		cout << "3. Вывести верхний элемент" << endl;
 		cout << "4. Узнать, пуста ли очередь" << endl;
+		cout << "5. Узнать, полна ли очередь" << endl;
 		cout << "0. Выйти\n\n";
-		cout << "Номер команды > "; cin >> number;
+		cout << "Номер команды: ";
+		cin >> number;
+
 		switch (number)
 		{
 		case '1': Add(&Q);
@@ -77,6 +100,11 @@ void main() //главная функция
 		case '4':
 			if (IsEmpty(&Q)) cout << endl << "Очередь пуста\n\n";
 			else cout << "\nОчередь не пуста" << "\n\n";
+			break;
+			//-----------------------------------------------
+		case '5':
+			if (IsFull(&Q)) cout << endl << "Очередь полна\n\n";
+			else { cout << "\nОчередь не полна\n\n"; }
 			break;
 			//-----------------------------------------------
 		case '0': break;
